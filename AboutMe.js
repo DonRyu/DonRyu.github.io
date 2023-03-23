86f748df2e479cb9c60d6f8e9a3165e482d7085f
Don Ryu <60118440+DonRyu@users.noreply.github.com>
2023-03-23 13:01:40 -0700

    Add files via upload

---
+++ AboutMe.js
@@ -0,0 +1,10 @@
+class Don_Ryu{
+    type = ['Full Stack Developer'];
+    languages = ['TS','JS','Java','C#']
+    libraries_and_frameworks = ['React','React Native','Next','Express']
+    database = ['Postgresql','MongoDB']
+    others = ['AWS','Node','Jest']
+    likes = ['Technical Challenges']
+}
+
+
