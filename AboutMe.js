4b07b113d5295121de57fb3a8ccb03b673686b5d
Dongheon Ryu <60118440+DonRyu@users.noreply.github.com>
2023-04-16 14:13:44 -0700

    Update AboutMe.js

--- AboutMe.js
+++ AboutMe.js
@@ -1,7 +1,7 @@
 class Don_Ryu{
     type = ['Full Stack Developer'];
     languages = ['TS','JS','Java','C#']
-    libraries_and_frameworks = ['React','React Native','Next','Express']
+    libraries_and_frameworks = ['React','React Native','Next','Express','.Net']
     database = ['Postgresql','MongoDB']
     others = ['AWS','Node','Jest']
     likes = ['Technical Challenges']
