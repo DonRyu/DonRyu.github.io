ced723ea7819035b3654433f666415dffabda98b
Don Ryu <60118440+DonRyu@users.noreply.github.com>
2023-09-11 15:52:48 -0700

    Update AboutMe.js

--- AboutMe.js
+++ AboutMe.js
@@ -2,7 +2,7 @@ class Don_Ryu{
     type = ['Full Stack Developer'];
     languages = ['TS','Python']
     libraries_and_frameworks = ['React','React Native','Next','Express']
-    database = ['MySQL','MongoDB','MS SQL]
+    database = ['MySQL','MongoDB','MS SQL']
     others = ['AWS','Node','Ubuntu']
 }
 
