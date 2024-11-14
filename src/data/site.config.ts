e43f67d46f14a477d511286a5918f179a7a7b04c
Don Ryu <alpharyu1031@gmail.com>
2024-11-13 21:19:34 -0800

    index change

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,10 +9,10 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'Mike Hoang',
-	title: 'Mike Hoang - Software Developer',
+	author: 'Don Ryu',
+	title: 'Don Ryu - Software Developer',
 	description:
-		'Mike Hoang is a software developer with a passion for creating innovative web applications, who always strive to make the complex seem effortless.',
+		'Don Ryu is a software developer with a passion for creating innovative web applications, who always strive to make the complex seem effortless.',
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
