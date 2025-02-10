490636cabb8ae0964c2b3607ab2fc2317ed9a54c
Don Ryu <alpharyu1031@gmail.com>
2025-02-09 20:25:20 -0800

    fix mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,37 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import remarkReadingTime from './src/utils/readTime.mjs';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://donRyu.github.io', 
-  vite: {
-    build: {
-      rollupOptions: {}
-    }
-  },
+	vite: {
+		build: {
+			rollupOptions: {}
+		}
+	},
 
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    drafts: true,
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true
-    }
-  },
-
-  integrations: [
-    mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: {
-        theme: 'material-theme-palenight',
-        wrap: true
-      },
-      drafts: true
-    }),
-    sitemap(),
-    tailwind()
-  ]
-});
+	site: 'https://donryu.github.io/',
+	// Write here your website url
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				theme: 'material-theme-palenight',
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
+})
